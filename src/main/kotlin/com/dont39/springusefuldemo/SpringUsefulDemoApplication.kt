5e3a9ca09fab6981863c3d693ad98bdd3de19c9f
sirius.se <sirius.se@ccpgames.com>
2019-05-24 16:37:28 +0800

    Initial commit

---
+++ src/main/kotlin/com/dont39/springusefuldemo/SpringUsefulDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.dont39.springusefuldemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringUsefulDemoApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringUsefulDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/dont39/springusefuldemo/SpringUsefulDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dont39.springusefuldemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringUsefulDemoApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}

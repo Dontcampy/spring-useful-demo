46afb8a5efedaea2263ed4ce53b6bfc03610811b
sirius.se <sirius.se@ccpgames.com>
2019-05-30 15:47:31 +0800

    Add custom cache error handler

--- src/main/kotlin/com/dont39/springusefuldemo/config/CacheConfig.kt
+++ src/main/kotlin/com/dont39/springusefuldemo/config/CacheConfig.kt
@@ -5,10 +5,15 @@ import com.dont39.springusefuldemo.AllOpen
 import com.dont39.springusefuldemo.CACHE_MANAGER_LOCAL
 import com.dont39.springusefuldemo.CACHE_PLAYER_ID
 import com.dont39.springusefuldemo.entity.PlayerEntity
+import com.dont39.springusefuldemo.util.lzWarn
 import com.fasterxml.jackson.core.type.TypeReference
+import org.slf4j.LoggerFactory
+import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.CachingConfigurerSupport
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.cache.interceptor.CacheErrorHandler
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -18,6 +23,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.lang.RuntimeException
 import java.time.Duration
 
 /**
@@ -27,6 +33,37 @@ import java.time.Duration
 @Configuration
 @AllOpen
 class CacheConfig : CachingConfigurerSupport() {
+
+  private class RelaxedCacheErrorHandler : SimpleCacheErrorHandler() {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    override fun handleCacheGetError(exception: RuntimeException, cache: Cache, key: Any) {
+      simpleLogCacheError("get", exception)
+    }
+
+    override fun handleCacheClearError(exception: RuntimeException, cache: Cache) {
+      simpleLogCacheError("clear", exception)
+
+    }
+
+    override fun handleCachePutError(exception: RuntimeException, cache: Cache, key: Any, value: Any?) {
+      simpleLogCacheError("put", exception)
+
+    }
+
+    override fun handleCacheEvictError(exception: RuntimeException, cache: Cache, key: Any) {
+      simpleLogCacheError("evict", exception)
+
+    }
+
+    private fun simpleLogCacheError(key: String, exception: RuntimeException) {
+      log.lzWarn { "Error [$key] from redis cache, error=[${exception.javaClass.simpleName}]-${exception.message}" }
+    }
+  }
+
+  override fun errorHandler(): CacheErrorHandler? {
+    return RelaxedCacheErrorHandler()
+  }
 
   object ListPlayerEntityTypeRef: TypeReference<List<PlayerEntity>>()
 
